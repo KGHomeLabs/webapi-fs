64b7ab83dbe576324a7a7a55980a7bad39377070
Home lab builder <NoPlan422@gmail.com>
2025-06-28 02:36:52 -0400

    the first gramm of sql relates to #81

---
+++ DB/dbuser.sql
@@ -0,0 +1,21 @@
+-- Make sure that you logon as an entra user with the necessary permissions to create users and roles.
+-- Create database user mapped to the App Service's Managed Identity
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    -- Create user (not rollback-safe)
+    CREATE USER [homelabuser-web-app] FROM EXTERNAL PROVIDER;
+
+    -- These are rollback-safe
+    ALTER ROLE db_datareader ADD MEMBER [homelabuser-web-app];
+    ALTER ROLE db_datawriter ADD MEMBER [homelabuser-web-app];
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    -- Optionally re-raise error
+    THROW;
+END CATCH
