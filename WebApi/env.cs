25aa70b693cb8deeebb283c744cf1b4f1597ee55
Home lab builder <NoPlan422@gmail.com>
2025-07-08 18:17:13 -0400

    made the startup a bit more env aware relates to #9

---
+++ WebApi/env.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebApi
+{
+   public static class EnvironmentExtension
+    {
+        public static bool  IsDev(this IWebHostEnvironment env)
+        {
+            return env.IsDevelopment() || env.EnvironmentName == "Development" || env.EnvironmentName == "Local";
+        }
+
+        public static bool IsProd(this IWebHostEnvironment env)
+        {
+            return env.IsProduction() || env.EnvironmentName.ToLower() == "Prod".ToLower();
+        }
+
+        public static bool IsPreview(this IWebHostEnvironment env)
+        {
+            return env.IsStaging() || env.EnvironmentName.ToLower() == "Prev".ToLower();
+        }
+    }
+}
